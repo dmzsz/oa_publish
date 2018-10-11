beb17ea74c61ce16e7c380bec68420fabfd0f956
dmzsz <dmzsz@sina.com>
2018-10-11 10:22:48 +0800

    t1 show 保证只有10条

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -28,55 +28,100 @@ $(document).ready(function () {
     }
 });
 
+var vessel = {
+    //len: 多余len行开始滚动
+    doScroll: function (len) {
+        var $parent = $('.table > tbody');
+        var $first = $parent.find('tr:first');
+        var height = $first.find('a').css('line-height');
 
-//len: 多余len行开始滚动
-var doscroll2 = function (len) {
-    var $parent = $('.table > tbody');
-    var $first = $parent.find('tr:first');
-    var height = $first.find('a').css('line-height');
+        $first.children('td').css("padding-top", 0);
+        $first.children('td').css("padding-bottom", 0);
 
-    $first.children('td').css("padding-top", 0);
-    $first.children('td').css("padding-bottom", 0);
-
-    $first.children('td')
-        .not(':has(div.td-slider)')
-        .wrapInner('<div class="td-slider" />');
-
-    if ($(".vessels-table tbody tr").length > len) {
         $first.children('td')
-            .children(".td-slider")
-            .slideUp(
-                1000,
-                function () {
-                    $(this).closest('tr').appendTo($parent);
-                    $(this).show();
-                });
-    };
+            .not(':has(div.td-slider)')
+            .wrapInner('<div class="td-slider" />');
 
-};
+        if ($(".vessels-table tbody tr").length > len) {
+            $first.children('td')
+                .children(".td-slider")
+                .slideUp(
+                    1000,
+                    function () {
+                        $(this).closest('tr').appendTo($parent);
+                        $(this).show();
+                    });
+        };
 
-var setColor = function () {
-    $('tbody tr td:nth-child(4)').each(function () {
-        var OpDate = new Date($(this).text());
-        var tr = $(this).closest('tr');
-        var CloseDate = new Date(tr.find("td:nth-child(5)").text());
-        var CurrentDate = new Date();
-        if (OpDate <= CurrentDate && CloseDate > CurrentDate) {
-            tr.css("color", "#7cfc00");
-        }
-    });
+    },
 
-    $('tbody tr td:nth-child(5)').each(function () {
-        var CloseDate = new Date($(this).text());
-        if (CloseDate <= new Date()) {
-            $(this).closest('tr').css("color", "#ffff00");
-        }
-    });
+    setColor: function () {
+        $('tbody tr td:nth-child(4)').each(function () {
+            var OpDate = moment($(this).text(), 'YYYY/MM/DD HH:mm');
+            var tr = $(this).closest('tr');
+            var CloseDate = moment(tr.find("td:nth-child(5)").text(), 'YYYY/MM/DD HH:mm');
+            var CurrentDate = moment().format('YYYY/MM/DD HH:mm');
+            if (OpDate <= CurrentDate && CloseDate > CurrentDate) {
+                tr.css("color", "#98ff35");
+            }
+        });
 
-    $('tbody tr td:nth-child(6)').each(function () {
-        var ETD = new Date($(this).text());
-        if (ETD == new Date()) {
-            $(this).closest('tr').css("color", "#ff0000");
-        }
-    });
+        $('tbody tr td:nth-child(5)').each(function () {
+            var CloseDate = moment($(this).text(), 'YYYY/MM/DD HH:mm');
+            if (CloseDate <= moment().format('YYYY/MM/DD HH:mm')) {
+                $(this).closest('tr').css("color", "#ffff33");
+            }
+        });
+
+        $('tbody tr td:nth-child(6)').each(function () {
+            var ETD = moment($(this).text(), 'YYYY/MM/DD');
+            var day = moment().format('YYYY/MM/DD');
+            if (ETD.isValid() && ETD.isSame(day)) {
+                $(this).closest('tr').css("color", "#ff3333");
+            }
+        });
+    }
+}
+
+var t1_data = {
+    clock: function () {
+        var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var dayNames = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+
+        // Create a newDate() object
+        var newDate = new Date();
+
+        // Extract the current date from Date object
+        newDate.setDate(newDate.getDate());
+
+        // Output the day, date, month and year
+        $('#date').html(newDate.getFullYear() + ' 年 ' + (newDate.getMonth() + 1) + ' 月 ' + newDate.getDate() + ' 日 ' + dayNames[newDate.getDay()]);
+
+        setInterval(function () {
+
+            // Create a newDate() object and extract the seconds of the current time on the visitor's
+            var seconds = new Date().getSeconds();
+
+            // Add a leading zero to seconds value
+            $("#sec").html((seconds < 10 ? "0" : "") + seconds);
+        }, 1000);
+
+        setInterval(function () {
+
+            // Create a newDate() object and extract the minutes of the current time on the visitor's
+            var minutes = new Date().getMinutes();
+
+            // Add a leading zero to the minutes value
+            $("#min").html((minutes < 10 ? "0" : "") + minutes);
+        }, 1000);
+
+        setInterval(function () {
+
+            // Create a newDate() object and extract the hours of the current time on the visitor's
+            var hours = new Date().getHours();
+
+            // Add a leading zero to the hours value
+            $("#hours").html((hours < 10 ? "0" : "") + hours);
+        }, 1000);
+    }
 }
